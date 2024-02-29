1f8c4ec7a5a934b041ba7b710456a701c7fc225e
Jorge <jorgecuadradovelasquez@gmail.com>
2024-02-28 20:55:51 -0500

    adding comments

--- retosPython/punto3.py
+++ retosPython/punto3.py
@@ -1,4 +1,4 @@
-#primos
+#retorna una lista con los primos de una lista de numeros, para esto primero halla la raiz del numero de la lista que se quiere verificar ya que por teorema un numero no se puede dividir por un numero mayor a su raiz cuadrada, luego recorre los numeros desde 2 hasta la raiz del numero y si el numero es divisible por alguno de estos numeros no es primo, si no es divisible por ninguno de estos numeros es primo
 from math import sqrt
 def primos(numeros):
     primos=[]
