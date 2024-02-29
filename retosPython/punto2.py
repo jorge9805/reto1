3283ebe22732700c345a8bd763d662179729bd3b
Jorge <jorgecuadradovelasquez@gmail.com>
2024-02-28 20:51:54 -0500

    took off the spaces and added some examples

--- retosPython/punto2.py
+++ retosPython/punto2.py
@@ -2,8 +2,10 @@
 #primero formatea la palabra a minuscula luego recorre las letras de la palabra y compara si la letra en la posicion i es igual a la letra en la posicion -i-1 si no es igual retorna falso si todas las letras son iguales retorna verdadero
 def palindromo(palabra):
     palabra = palabra.lower()
+    palabra = palabra.replace(" ", "")
     for i in range(len(palabra)):
         if palabra[i] != palabra[-i-1]:
             return False
     return True
 print(palindromo("Anita lava la tina"))
+print(palindromo("mahalo"))
